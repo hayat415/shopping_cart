dcbb7098059d1d68e6e127180fa030b18efed3c4
hayat <hayatshah415@gmail.com>
2022-07-30 16:21:01 +0500

    final structure

---
+++ ecomapp/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from .views import *
+
+
+app_name="ecomapp"
+urlpatterns = [
+    path("",HomeViews.as_view(), name="home"),
+    path("allproduct/", AllProductsView.as_view(), name="allproduct"),
+    path("productdetail/<slug:slug>/", ProductDetailView.as_view(), name="productdetail"),
+    path("add-to-cart-<int:pro_id>/", AddToCartView.as_view(), name="addtocart"),
+    path("my-cart/", MyCartView.as_view(), name="mycart"),
+    path("manage-cart/<int:cp_id>/", ManageCartView.as_view(), name="managecart"),
+    path("empty-cart/", EmptyCartView.as_view(), name="emptycart"),
+    path("checkout/", CheckoutView. as_view(), name="checkoutview"),
+    path("register/",CustomerRegistratonView.as_view(),name="customerregistration"),
+    path("logout/", CustomerLogoutView.as_view(), name="cutomerlogout"),
+    path("login/", CustomerLoginView.as_view(), name="customerlogin"),
+    path("profile/", CustomerProfileView.as_view(), name="customerprofile"),
+    path("profile/order-<int:pk>/", CustomerOrderDetailView.as_view(), name="customerorderdetail"),
+    path("admin-login/", AdminLoginView.as_view(), name="adminlogin"),
+    path("admin-home/", AdminHomeView.as_view(), name="adminhome"),
+    path("admin-order/<int:pk>/", AdminOrderDetailView.as_view(), name="adminorderdetail"),
+    path("admin-all-orders/", AdminOrderListView.as_view(), name="adminorderlist"),
+    path("admin-order-<int:pk>-change/", AdminOrderStatusChangeView.as_view(), name="adminorderstatuschange"),
+    path("search/", SearchView.as_view(), name="search"),
+    path("easypaisa-request/", EasypaisaRequestView.as_view(), name="easypaisarequest"),
+]
